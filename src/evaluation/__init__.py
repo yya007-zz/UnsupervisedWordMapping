37f75e50e21852d56b0ca21e87e3fe8a9c5bab40
Yuang Yao <yaoyuang1996@gmail.com>
2017-12-12 17:12:23 -0500

    FixCycleGAN

--- src/evaluation/__init__.py
+++ src/evaluation/__init__.py
@@ -2,3 +2,4 @@ from .wordsim import get_wordsim_scores, get_crosslingual_wordsim_scores, get_wo
 from .word_translation import get_word_translation_accuracy
 from .sent_translation import get_sent_translation_accuracy, load_europarl_data
 from .evaluator import Evaluator
+from .evaluator_Cycle import Evaluator_Cycle

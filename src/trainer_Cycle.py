ee91fa5b92032ffdca1e79478dd9dbe7c0f6464b
Yuang Yao <yaoyuang1996@gmail.com>
2017-12-13 03:46:01 -0500

    FixCycleGAN

--- src/trainer_Cycle.py
+++ src/trainer_Cycle.py
@@ -171,8 +171,9 @@ class Trainer_Cycle(object):
         
         map_loss = F.binary_cross_entropy(preds, 1 - y)
         map_loss = self.params.dis_lambda * map_loss
-
+        print(map_loss)
         loss = map_loss + self.total_consistency_loss(volatile=False)
+        print(loss)
         # check NaN
         if (loss != loss).data.any():
             logger.error("NaN detected (fool discriminator)")

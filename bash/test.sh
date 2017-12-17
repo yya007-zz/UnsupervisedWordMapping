4035f3854d4d30987a8719c2fd356a9f62a0e9aa
Yuang Yao <yaoyuang1996@gmail.com>
2017-12-17 01:26:42 -0500

    FixCycleGAN

---
+++ bash/test.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+python ../unsupervised.py --n_epochs 2 --n_iters 2 --epoch_size 10000 --src_lang en --tgt_lang it --src_emb data/pretrained/en.vec --tgt_emb data/pretrained/it.vec --refinement True

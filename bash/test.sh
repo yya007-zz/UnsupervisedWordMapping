fb54723fb6edaa010333a017d7d4f4750233f291
Yuang Yao <yaoyuang1996@gmail.com>
2017-12-17 02:13:23 -0500

    FixCycleGAN

--- bash/test.sh
+++ bash/test.sh
@@ -1,2 +1,3 @@
 #! /bin/bash
+git pull
 python unsupervised.py --n_epochs 2 --n_iters 2 --map_optimizer "sgd,lr=0.01" --epoch_size 10000 --src_lang en --tgt_lang it --src_emb data/pretrained/en.vec --tgt_emb data/pretrained/it.vec --refinement True

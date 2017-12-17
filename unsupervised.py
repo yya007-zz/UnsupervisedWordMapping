4b3910152f4b6b4fc5d286e1f8d88caa7ce1bdbd
Yuang Yao <yaoyuang1996@gmail.com>
2017-12-17 04:20:43 -0500

    newExp

--- unsupervised.py
+++ unsupervised.py
@@ -300,9 +300,8 @@ if params.adversarial:
     update_plot_info(to_log1, "_t_train_best")
     update_plot_info(to_log2, "_f_train_best")
 
-    address=os.path.join(params.exp_path, 'plot_info.test')
-
     if params.quick_test:
+        address=os.path.join(params.exp_path, 'plot_info.test')
         with open(address, 'w') as outfile:  
             json.dump(plot_info, outfile)
 

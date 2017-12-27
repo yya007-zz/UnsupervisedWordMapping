8936621e0e6092016ebbdee29bf8fa15b4fc9e13
Yuang Yao <yaoyuang1996@gmail.com>
2017-12-27 02:34:16 -0500

    clear

--- bash/test.sh
+++ bash/test.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 git pull
-python unsupervised.py --quick_test True --cc.method 'csls_knn_10' --n_epochs 2 --n_iters 2 --map_optimizer "sgd,lr=0.01" --epoch_size 10000 --src_lang en --tgt_lang it --src_emb data/pretrained/en.vec --tgt_emb data/pretrained/it.vec --refinement True
+python unsupervised.py --quick_test True --cc_method 'csls_knn_10' --n_epochs 2 --n_iters 2 --map_optimizer "sgd,lr=0.01" --epoch_size 10000 --src_lang en --tgt_lang it --src_emb data/pretrained/en.vec --tgt_emb data/pretrained/it.vec --refinement True
